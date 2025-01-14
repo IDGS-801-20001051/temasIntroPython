bebb967d4497df3dfe37c001f9e749b3b8be31ee
Leonardo7 <fabian.anrey@outlook.com>
2025-01-13 18:55:45 -0600

    tercer commit

---
+++ 01-variables.py
@@ -0,0 +1,12 @@
+print('Ejemplo1 : ')
+num1 = 5
+num2 = 4
+res = num1 + num2
+print(res)
+
+print('Dame dos numero para sumarlos')
+print('')
+num1=int(input('Ingrese un numero:'))
+num2=int(input('Ingrese el segundo:'))
+suma=num1 +num2
+print('la suma de {1} más {2} es: {0} '.format(suma,num1,num2))
---
+++ 02-ifelse.py
@@ -0,0 +1,7 @@
+num = 3
+num2= 8
+
+if num > num2:
+    print(num,'is greater than', num2)
+else:
+    print(num2,'is greater than',num)
